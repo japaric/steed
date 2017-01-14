33cc81e9520f94e1eeefef1f4467f65aa3d24184
Jorge Aparicio <japaricious@gmail.com>
2017-01-14 17:23:50 -0500

    ci: use a stable cross release

--- ci/install.sh
+++ ci/install.sh
@@ -4,18 +4,12 @@ main() {
     curl https://sh.rustup.rs -sSf | \
         sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
 
-    source ~/.cargo/env
-
-    pushd ..
-    which cross || cargo install --git https://github.com/japaric/cross
-    popd
-
-    # curl -LSfs https://japaric.github.io/trust/install.sh | \
-    #     sh -s -- \
-    #        --force \
-    #        --git japaric/cross \
-    #        --tag v0.1.5 \
-    #        --target x86_64-unknown-linux-gnu
+    curl -LSfs https://japaric.github.io/trust/install.sh | \
+        sh -s -- \
+           --force \
+           --git japaric/cross \
+           --tag v0.1.6 \
+           --target x86_64-unknown-linux-gnu
 }
 
 main
