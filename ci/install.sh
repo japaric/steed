b80c29e684231e04642f17d2c55d9607255a6018
Jorge Aparicio <japaricious@gmail.com>
2017-01-13 20:40:31 -0500

    use git version of cross

--- ci/install.sh
+++ ci/install.sh
@@ -4,12 +4,14 @@ main() {
     curl https://sh.rustup.rs -sSf | \
         sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
 
-    curl -LSfs https://japaric.github.io/trust/install.sh | \
-        sh -s -- \
-           --force \
-           --git japaric/cross \
-           --tag v0.1.4 \
-           --target x86_64-unknown-linux-gnu
+    cargo install -f --git https://github.com/japaric/cross
+
+    # curl -LSfs https://japaric.github.io/trust/install.sh | \
+    #     sh -s -- \
+    #        --force \
+    #        --git japaric/cross \
+    #        --tag v0.1.5 \
+    #        --target x86_64-unknown-linux-gnu
 }
 
 main
