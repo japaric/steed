42b29282a812bd26202f4f8b82e539775dbe918b
Tobias Bucher <tobiasbucher5991@gmail.com>
2017-02-07 12:39:42 +0100

    `sys_common` is now closer to upstream

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -34,27 +34,3 @@ pub use self::imp::*;
 
 #[path = "linux/mod.rs"]
 mod imp;
-
-
-#[doc(hidden)]
-pub trait AsInner<Inner: ?Sized> {
-    fn as_inner(&self) -> &Inner;
-}
-
-/// A trait for viewing representations from std types
-#[doc(hidden)]
-pub trait AsInnerMut<Inner: ?Sized> {
-    fn as_inner_mut(&mut self) -> &mut Inner;
-}
-
-/// A trait for extracting representations from std types
-#[doc(hidden)]
-pub trait IntoInner<Inner> {
-    fn into_inner(self) -> Inner;
-}
-
-/// A trait for creating std types from internal representations
-#[doc(hidden)]
-pub trait FromInner<Inner> {
-    fn from_inner(inner: Inner) -> Self;
-}
--- src/sys_common/mod.rs
+++ src/sys_common/mod.rs
@@ -1,4 +1,31 @@
-pub use sys::*;
-
+// Rust 1.15.0
+pub mod io;
 // Rust 1.15.0 (close)
 pub mod net;
+
+/// A trait for viewing representations from std types
+#[doc(hidden)]
+pub trait AsInner<Inner: ?Sized> {
+    fn as_inner(&self) -> &Inner;
+}
+
+/// A trait for viewing representations from std types
+#[doc(hidden)]
+pub trait AsInnerMut<Inner: ?Sized> {
+    fn as_inner_mut(&mut self) -> &mut Inner;
+}
+
+/// A trait for extracting representations from std types
+#[doc(hidden)]
+pub trait IntoInner<Inner> {
+    fn into_inner(self) -> Inner;
+}
+
+/// A trait for creating std types from internal representations
+#[doc(hidden)]
+pub trait FromInner<Inner> {
+    fn from_inner(inner: Inner) -> Self;
+}
+
+/// One-time runtime cleanup.
+pub fn cleanup() { }
