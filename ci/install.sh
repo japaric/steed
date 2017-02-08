6c19b65cf23aae46987a2b3f296c018113e79115
Jorge Aparicio <japaricious@gmail.com>
2017-02-08 00:21:35 -0500

    move to Cross v0.1.9
    closes #97

--- ci/install.sh
+++ ci/install.sh
@@ -8,7 +8,7 @@ main() {
         sh -s -- \
            --force \
            --git japaric/cross \
-           --tag v0.1.8 \
+           --tag v0.1.9 \
            --target x86_64-unknown-linux-gnu
 }
 
