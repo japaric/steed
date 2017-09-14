7439fc9b4b678bd18357dc1dacbdcf8868e18991
Jorge Aparicio <jorge@japaric.io>
2017-09-14 21:24:12 +0200

    always use the latest stable cross release
    closes #152

--- ci/install.sh
+++ ci/install.sh
@@ -1,11 +1,18 @@
 set -ex
 
 main() {
+    # This fetches latest stable release
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+                    | cut -d/ -f3 \
+                    | grep -E '^v[0.1.0-9.]+$' \
+                    | sort --version-sort \
+                    | tail -n1)
+
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
            --git japaric/cross \
-           --tag v0.1.10 \
+           --tag $tag \
            --target x86_64-unknown-linux-musl
 }
 
